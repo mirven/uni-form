06cdda3daca7f36c533f281f2ce499846fa9b494
Christoph Thiel <ct@kki.org>
2009-04-16 19:11:13 +0800

    merge some parts of install.rb from uni_form_patch (http://rails-uni-form-patch.googlecode.com/svn/trunk/uni_form_patch/)
    Signed-off-by: Marcus Irven <marcus@marcusirven.com>

--- install.rb
+++ install.rb
@@ -1,1 +1,35 @@
-# Install hook code here
+require 'fileutils'
+
+RAILS_ROOT = File.join(File.dirname(__FILE__), '../../../')
+
+#####################################
+# Copy resource files
+#####################################
+
+FileUtils.cp( 
+  File.join(File.dirname(__FILE__), 'resources', 'public', 'stylesheets', 'uni-form.css'), 
+  File.join(RAILS_ROOT, 'public', 'stylesheets'),
+  :verbose => true
+)
+
+FileUtils.cp( 
+  File.join(File.dirname(__FILE__), 'resources', 'public', 'stylesheets', 'uni-form-generic.css'), 
+  File.join(RAILS_ROOT, 'public', 'stylesheets'),
+  :verbose => true
+)
+
+FileUtils.cp(
+  File.join(File.dirname(__FILE__), 'resources', 'public', 'javascripts', 'uni-form.prototype.js'),
+  File.join(RAILS_ROOT, 'public', 'javascripts'),
+  :verbose => true
+)
+
+p ''
+p '========== Installation of Uni Form is completed =========='
+p ''
+
+#####################################
+# Show the README text file
+#####################################
+puts IO.read(File.join(File.dirname(__FILE__), 'README'))
+
